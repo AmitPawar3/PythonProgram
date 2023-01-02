fcbeba0b8a98bd48c04f2efb8910ec40438e32d1
AmitPawar3 <110882914+AmitPawar3@users.noreply.github.com>
2023-01-02 23:16:49 +0530

    Create SumTwoNo.py

---
+++ SumTwoNo.py
@@ -0,0 +1,13 @@
+# Prompt the user to enter two numbers
+num1 = input("Enter a number: ")
+num2 = input("Enter another number: ")
+
+# Convert the input strings to integers
+num1 = int(num1)
+num2 = int(num2)
+
+# Calculate the sum of the two numbers
+sum = num1 + num2
+
+# Print the sum
+print("The sum of", num1, "and", num2, "is", sum)
